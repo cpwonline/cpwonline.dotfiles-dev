66d6992f02db7796c350c98c6a5c44a23a7e7002
Jose F Rivas C <josefelixrc7@gmail.com>
2022-01-12 18:30:49 +0000

    feat: Fix some things into openbox, rofi and tint2

--- etc/cpwos/dev/rofi/volume/volume.sh
+++ etc/cpwos/dev/rofi/volume/volume.sh
@@ -2,16 +2,14 @@
 
 rofi_command="rofi -theme /etc/cpwos/dev/rofi/volume/volume.rasi"
 
-## Get Volume
-MUTE=$(amixer get Master | tail -n 1 | awk -F ' ' '{print $6}' | tr -d '[]%')
+# Volume state
+mute_state=$(amixer get Master | tail -n 1 | awk -F ' ' '{print $6}' | tr -d '[]%')
+volume_level="$(amixer get Master | tail -n 1 | awk -F ' ' '{print $5}' | tr -d '[]%')%"
 
-active=""
-urgent=""
-
-if [[ $MUTE == *"on"* ]]; then
-    VOLUME="$(amixer get Master | tail -n 1 | awk -F ' ' '{print $5}' | tr -d '[]%')%"
+if [[ $mute_state == *"on"*  ]]; then
+	volume_final=$volume_level
 else
-    VOLUME="Mute"
+	volume_final="Muted"
 fi
 
 ## Icons
@@ -20,23 +18,23 @@ volume_down_text="Volume Down"
 volume_mute_text="Mute"
 
 volume_up="$volume_up_text\0icon\x1faudio-volume-high"
-volume_down="$volume_down_text\0icon\x1f/usr/share/cpwos/reboot.png"
-volume_mute="$volume_mute_text\0icon\x1f/usr/share/cpwos/lock.png"
+volume_down="$volume_down_text\0icon\x1faudio-volume-low"
+volume_mute="$volume_mute_text\0icon\x1faudio-volume-muted"
 
 # Arrays
 options="$volume_up\n$volume_down\n$volume_mute"
 
 ## Main
-chosen="$(echo -e "$options" | $rofi_command -p "$VOLUME" -dmenu -selected-row 0)"
+chosen="$(echo -e "$options" | $rofi_command -p "$volume_final" -dmenu -selected-row 0)"
 case $chosen in
 	$volume_up_text)
-		amixer -Mq set Master,0 5%+ unmute && notify-send -u low -t 1500 "+5% Volume "
+		/usr/bin/cpwos/volume_up.sh
 		;;
 	$volume_down_text)
-		amixer -Mq set Master,0 5%- unmute && notify-send -u low -t 1500 "-5% Volume"
+		/usr/bin/cpwos/volume_down.sh
 		;;
 	$volume_mute_text)
-		amixer -q set Master toggle && notify-send -u low -t 1500 "Mute enable/disable"
+		/usr/bin/cpwos/volume_mute.sh
 		;;
 esac
 
